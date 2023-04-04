194d66698b6add0161ca3e9c4d6a3d6d960bd662
Mohit Singh <mohit@mohitsingh.in>
2023-04-04 12:52:49 +0530

    refactor: cleanup and put lsp behind experimental

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,21 @@
 import * as path from 'path'
 import * as vscode from 'vscode'
-
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-  TransportKind,
-} from 'vscode-languageclient/node'
+import { LanguageClient } from 'vscode-languageclient/node'
 
 let client: LanguageClient
 
-export function activate(context: vscode.ExtensionContext) {
-  const serverPath = vscode.workspace
-    .getConfiguration('markdoc')
-    .get('serverPath') as string
+export async function activate(context: vscode.ExtensionContext) {
+  const config = vscode.workspace.getConfiguration('markdoc')
+  const shouldRunLsp = config.get('experimental') as boolean
+  if (!shouldRunLsp) return
+
+  const serverPath = config.get('serverPath') as string
 
   const serverModule = serverPath
     ? path.resolve(serverPath)
     : context.asAbsolutePath(
         path.join('node_modules', 'markdoc-ls', 'bin', 'markdoc-ls')
       )
-  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] }
-
-  const serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  }
-
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: 'file', language: 'markdoc' }],
-  }
 
   vscode.commands.registerCommand('markdoc.restart', async () => {
     try {
@@ -44,18 +26,17 @@ export function activate(context: vscode.ExtensionContext) {
   })
 
   client = new LanguageClient(
-    'markdocLanguageServer',
+    'markdoc-ls',
     'Markdoc Language Server',
-    serverOptions,
-    clientOptions
+    { module: serverModule },
+    { documentSelector: [{ scheme: 'file', language: 'markdoc' }] }
   )
 
-  client.start()
+  await client.start()
 }
 
-export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined
+export async function deactivate(): Promise<void> | undefined {
+  if (client) {
+    await client.stop()
   }
-  return client.stop()
 }
